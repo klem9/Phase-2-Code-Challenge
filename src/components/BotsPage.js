9630bf157e040a30246b489c466121219150ecb5
Klemense <klemenseloh@gmail.com>
2022-03-04 10:43:03 -0500

    testing

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
-  //start here with your code for step one
+  const [bots, setBots] = useState([])
+  const [botArmy, setBotArmy] = useState([])
 
+  useEffect(() => {
+    fetch("http://localhost:6001/bots")
+    .then(resp => resp.json())
+    .then(botData => {
+        setBots(botData);
+    });
+    }, []);
+
+    function handleAddToBotArmy(bot){
+      const myBotArmy = botArmy.filter(item => bot.id === item.id)
+      if (myBotArmy.length <= 0)
+        setBotArmy([...botArmy, bot])
+    }
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy botArmy= {botArmy}/>
+      <BotCollection bots = {bots} onAddToBotArmy={handleAddToBotArmy}/>
     </div>
   )
 }
